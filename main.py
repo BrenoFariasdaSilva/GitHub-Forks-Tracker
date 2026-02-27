441c295b21f7239f44b1d49e64a67143c796dbd0
Breno Farias da Silva <breno.vicio78@gmail.com>
2026-02-27 13:41:40 -0300

    REFACTOR: Updating the Libraries Imports in the ./main.py

--- main.py
+++ main.py
@@ -28,12 +28,17 @@ Assumptions & Notes:
       `.env` or CLI to authenticate to GitHub's REST API.
 """
 
+
+import argparse  # For CLI parsing
 import atexit  # For playing a sound when the program finishes
 import datetime  # For getting the current date and time
 import os  # For running a command in the terminal
 import platform  # For getting the operating system name
 import sys  # For system-specific parameters and functions
 from colorama import Style  # For coloring the terminal
+from commits_diff import build_original_sha_set, process_single_fork   # For processing forks and computing divergent commits
+from dotenv import load_dotenv  # Load .env file when needed
+from github_api import GitHubAPI  # Core HTTP client for GitHub REST API
 from Logger import Logger  # For logging output to both terminal and file
 from pathlib import Path  # For handling file paths
 
